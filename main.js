94c5b070359ddf5db7d4ac0d67712e8bbf25ab47
Jeppe Olander <jeppe@jeppeolander.dk>
2023-04-05 20:40:21 +0200

    Revert "Changed all code to chatGPT"
    This reverts commit e2f0886c6b3e36232244d2378bda232fccc3eed7.

--- main.js
+++ main.js
@@ -1,37 +1,6 @@
-/* Smooth scrolling for internal links */
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top
-      }, 1000);
-    }
-  });
-  
-  /* Toggle mobile navigation */
-  $('.toggle-nav').click(function() {
-    $('.nav-links').slideToggle();
-  });
-  
-  /* Highlight current page in navigation */
-  $(function() {
-    var current = location.pathname;
-    $('.nav-links li a').each(function() {
-      var $this = $(this);
-      if ($this.attr('href').indexOf(current) !== -1) {
-        $this.addClass('active');
-      }
-    });
-  });
-  
-  /* Open links in new tab */
-  $('a[href^="http://"], a[href^="https://"]').attr('target','_blank');
-  
-  /* Smooth scrolling on page load */
-  $(document).ready(function(){
-    $('html, body').animate({
-        scrollTop: $('body').offset().top
-    }, 1000);
-  });
-  
+let toggle = document.querySelector("#header .toggle-button");
+let collapse = document.querySelectorAll("#header .collapse");
+
+toggle.addEventListener('click' , function(){
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"));
+})
